8ec7cbdd77d906c5623fe9e43fdc2b3d02bd3c00
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-01-28 00:08:14 +0100

    viral advertising algorithm

--- algorithms.js
+++ algorithms.js
@@ -302,3 +302,26 @@ function beautifulDays(i, j, k) {
   }
   return count;
 }
+
+
+/**
+ * VIRAL ADVERTISING
+ * HackerLand Enterprise is adopting a new viral advertising strategy. 
+ * When they launch a new product, they advertise it to exactly 5 people on social media.
+ * On the first day, half of those  people (i.e., floor(5/2) = 2) like the advertisement 
+ * and each shares it with 3 of their friends. At the beginning of the second day, 
+ * floor(5/2) * 3 = 6 people receive the advertisement.
+ * Each day, fllor(recipients/2) of the recipients like the advertisement 
+ * and will share it with 3 friends on the following day. 
+ * Assuming nobody receives the advertisement twice, 
+ * determine how many people have liked the ad by the end of a given day, beginning with launch day as day 1.
+ */
+function viralAdvertising(n) {
+  let sub = 5;
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    count += Math.floor(sub / 2);
+    sub = Math.floor(sub / 2) * 3;
+  }
+  return count;
+}
