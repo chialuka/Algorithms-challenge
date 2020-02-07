6b3826ec6a9c6509725d1f79868f780fdeb541dd
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-08 00:28:03 +0100

    drying potatoes

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -158,3 +158,29 @@ function noOdds( values ){
  * @param {Number} n 
  */
 const angle = (n) => 180 * (n - 2);
+
+
+
+/**
+ * DRYING POTATOES
+ * All we eat is water and dry matter.
+ * John bought potatoes: their weight is 100 kilograms. Potatoes contain water and dry matter.
+ * The water content is 99 percent of the total weight. 
+ * He thinks they are too wet and puts them in an oven - at low temperature - for them to lose some water.
+ * At the output the water content is only 98%.
+ * What is the total weight in kilograms (water content plus dry matter) coming out of the oven?
+ * He finds 50 kilograms and he thinks he made a mistake: "So much weight lost for such a small change in water content!"
+ * Can you help him?
+ * Write function potatoes with
+ * int parameter p0 - initial percent of water-
+ * int parameter w0 - initial weight -
+ * int parameter p1 - final percent of water -
+ * potatoes should return the final weight coming out of the oven w1 truncated as an int.
+ * @param {Number} p0 
+ * @param {Number} w0 
+ * @param {Number} p1 
+ */
+function potatoes(p0, w0, p1) {
+  const ans = ((w0 - (p0 * w0 / 100)) * 100) / (100 - p1);
+  return Math.floor(ans.toFixed(2));
+}
