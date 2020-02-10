782dccedf6716780eef05670fa21749087d2551e
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-10 22:27:08 +0100

    row of the odd triangle algorithm

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -319,3 +319,32 @@ function duplicateCount(text){
   }, {});
   return Object.values(ans).filter(x => x > 1).length;
 }
+
+
+/**
+ * ROW OF THE ODD TRIANGLE
+ * Given a triangle of consecutive odd numbers:
+ *             1
+ *          3     5
+ *       7     9    11
+ *   13    15    17    19
+ * 21    23    25    27    29
+ * find the triangle's row knowing its index (the rows are 1-indexed), e.g.:
+ * odd_row(1)  ==  [1]
+ * odd_row(2)  ==  [3, 5]
+ * odd_row(3)  ==  [7, 9, 11]
+ * Note: your code should be optimized to handle big inputs.
+ * @param {Number} n 
+ */
+function oddRow(n) {
+  const arr = [];
+  const prev = n - 1;
+  const last = (Math.pow(prev, 2) + (prev)) + 1;
+  arr.push(last);
+  let i = 0;
+  while (i < prev) {
+    arr.push(arr[i] + 2);
+    i++
+  }
+  return arr;
+}
