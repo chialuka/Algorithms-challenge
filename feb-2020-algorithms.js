44e1c25305836657083e5872a8afcd796d1df477
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-11 01:31:58 +0100

    potion class 101 algorithm

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -374,3 +374,38 @@ function createTiles(n){
   }
   return arr;
 }
+
+
+/**
+ * POTION CLASS 101
+ * This is your first potion class in Hogwarts 
+ * and professor gave you a homework to figure out what color potion will turn into 
+ * if he'll mix it with some other potion. 
+ * All potions have some color that written down as RGB color from [0, 0, 0] to [255, 255, 255]. 
+ * To make task more complicated teacher will do few mixing and after will ask you for final color. 
+ * Besides color you also need to figure out what volume will have potion after final mix.
+ * Based on your programming background you managed to figure 
+ * that after mixing two potions colors will mix as if mix two RGB colors. 
+ * For example, if you'll mix potion that have color [255, 255, 0] 
+ * and volume 10 with one that have color [0, 254, 0] and volume 5, 
+ * you'll get new potion with color [170, 255, 0] and volume 15. 
+ * So you decided to create a class Potion that will have two properties: color (a list with 3 integers) 
+ * and volume (a number), and one method mix that will accept another Potion and return a mixed Potion.
+ */
+class Potion {
+  constructor (arr, vol) {
+    this.color = [arr[0], arr[1], arr[2]];
+    this.volume = vol;
+  }
+  
+  
+  mix (potion) {
+    this.newVolume = this.volume + potion.volume;
+    const newRed = Math.ceil(((this.color[0] * this.volume) + (potion.color[0] * potion.volume)) / this.newVolume);
+    const newGreen = Math.ceil(((this.color[1] * this.volume) + (potion.color[1] * potion.volume)) / this.newVolume);
+    const newBlue = Math.ceil(((this.color[2] * this.volume) + (potion.color[2] * potion.volume)) / this.newVolume);
+    const colors = [newRed, newGreen, newBlue];
+    const ans = new Potion(colors, this.newVolume)
+    return ans;
+  }
+}
