c899c51cd2074c9128833000d19b2ee1953f9dc6
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-18 23:10:42 +0100

    looking for a benefactor

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -621,3 +621,20 @@ function pattern(n){
    }
    return output.slice(0, output.length - 1);
  }
+
+
+
+ /**
+  * LOOKING FOR A BENEFACTOR
+  * The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. 
+  * The association is funded through financial donations from generous benefactors. 
+  * John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] 
+  * He wants to know how much the next benefactor should give to the association 
+  * so that the average of the first n + 1 donations should reach an average of 30. 
+  * The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+  * Should the last donation be a non positive number (<= 0) John wants us to throw (or raise) an error
+  */
+ function newAvg(arr, newavg) {
+  const val = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0);
+  return val > 0 ? Math.ceil(val) : new TestError('TestError');
+}
