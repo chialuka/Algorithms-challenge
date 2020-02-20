32346b439cac0fd2c2d11c569a310ca6982daabc
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-20 23:37:15 +0100

    selective fear of numbers

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -693,3 +693,35 @@ function nameInStr(str, name){
   }
   return true;
 }
+
+
+
+/**
+ * SELECTIVE FEAR OF NUMBERS
+ * I've got a crazy mental illness. I dislike numbers a lot. 
+ * But it's a little complicated: The number I'm afraid of depends on which day of the week it is... 
+ * This is a concrete description of my mental illness:
+ * Monday --> 12
+ * Tuesday --> numbers greater than 95
+ * Wednesday --> 34
+ * Thursday --> 0
+ * Friday --> numbers divisible by 2
+ * Saturday --> 56
+ * Sunday --> 666 or -666
+ * Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
+ * 
+ * @param {String} day 
+ * @param {Number} num 
+ */
+const AmIAfraid = function(day, num){
+  const values = {
+    'Monday': (x) => x == 12,
+    'Tuesday': (x) => x > 95,
+    'Wednesday': (x) => x == 34,
+    'Thursday': (x) => x == 0,
+    'Friday': (x) => x % 2 == 0,
+    'Saturday': (x) => x == 56,
+    'Sunday': (x) => [666, -666].includes(x)
+  }
+  return values[day](num);
+}
