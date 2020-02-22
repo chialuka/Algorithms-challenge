3e57bd1b85e1276e3a965dcd63a6d8c3e47efd8d
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-02-23 00:05:25 +0100

    sort out the men from the boys

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -776,3 +776,24 @@ const twoOldestAges = (ages) => ages.sort((a, b) => a - b).slice(-2)
  * @param {Number} bound 
  */
 const maxMultiple = (divisor, bound) => Math.floor(bound / divisor) * divisor;
+
+
+
+/**
+ * SORT OUT THE MEN FROM THE BOYS
+ * Given an array/list [] of n integers , Separate The even numbers from the odds , or Separate the men from the boys !alt !alt
+ * Notes
+ * Return an array/list where Even numbers come first then odds
+ * Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+ * Array/list size is at least *4*** .
+ * Array/list numbers could be a mixture of positives , negatives .
+ * Have no fear , It is guaranteed that no Zeroes will exists . !alt
+ * Repetition of numbers in the array/list could occur , So (duplications are not included when separating).
+ * @param {Array} arr 
+ */
+function menFromBoys(arr){
+  const ar = [...new Set(arr)];
+  const odd = ar.filter((x, y , z) => x % 2 != 0).sort((a, b) => b - a);
+  const even = ar.filter((x, y, z) => x % 2 == 0).sort((a, b) => a - b);
+  return even.concat(odd);;
+}
