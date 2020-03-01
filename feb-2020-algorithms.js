c35a7dc7f17472a8b1bbef2f5807fd5297675a7f
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-02 00:00:31 +0100

    lottery machine

--- feb-2020-algorithms.js
+++ feb-2020-algorithms.js
@@ -1009,3 +1009,19 @@ function scrabbleScore(str){
  * @param {String} x 
  */
 const absentVowel = (x) => ['a', 'e', 'i', 'o', 'u'].findIndex(y => !x.includes(y))
+
+
+
+
+/**
+ * LOTTERY MACHINE
+ * Your task is to write an update for a lottery machine. 
+ * Its current version produces a sequence of random letters and integers (passed as a string to the function). 
+ * Your code must filter out all letters and return unique integers as a string, in their order of first appearance. 
+ * If there are no integers in the string return "One more run!"
+ * @param {String} str 
+ */
+const lottery = (str) => {
+  const ans = str.match(/[0-9]/g)
+  return ans ? [...new Set(ans)].join('') : 'One more run!';
+};
