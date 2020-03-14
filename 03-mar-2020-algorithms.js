2928b37239724c31c6cfbf780be0f571b765571d
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-14 23:00:15 +0100

    is he gonna survive

--- 03-mar-2020-algorithms.js
+++ 03-mar-2020-algorithms.js
@@ -318,3 +318,16 @@ function tripleTrouble(one, two, three){
   }
   return str;
  }
+
+
+/**
+ * IS HE GONNA SURVIVE
+ * A hero is on his way to the castle to complete his mission. 
+ * However, he's been told that the castle is surrounded with a couple of powerful dragons! 
+ * each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. 
+ * Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+ * Return True if yes, False otherwise :)
+ * @param {Number} bullets 
+ * @param {Number} dragons 
+ */
+const hero = (bullets, dragons) => bullets >= (dragons * 2)
