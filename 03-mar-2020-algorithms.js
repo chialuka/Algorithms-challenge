30efcf25059ffc46c91d0536edee4411bd769a00
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-17 23:52:42 +0100

    late ride

--- 03-mar-2020-algorithms.js
+++ 03-mar-2020-algorithms.js
@@ -349,3 +349,22 @@ const divisions = (n, divisor) => {
   }
   return count;
 };
+
+
+
+/**
+ * LATE RIDE
+ * One night you go for a ride on your motorcycle. 
+ * At 00:00 you start your engine, and the built-in timer automatically begins counting the length of your ride, in minutes. 
+ * Off you go to explore the neighborhood.
+ * When you finally decide to head back, you realize there's a chance the bridges on your route home are up, leaving you stranded! 
+ * Unfortunately, you don't have your watch on you and don't know what time it is. 
+ * All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+ * Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.
+ * @param {Number} n 
+ */
+function lateRide(n) {
+  const hh = Math.trunc(n / 60).toString();
+  const mm = (n % 60).toString();
+  return [...hh, ...mm].reduce((a, b) => +a + +b, 0);
+}
