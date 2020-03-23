e1b2e92f2c4c202c3f2ecc3e321a9163b79fbd06
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-23 23:51:55 +0100

    number to string

--- 03-mar-2020-algorithms.js
+++ 03-mar-2020-algorithms.js
@@ -439,3 +439,13 @@ function rowSumOddNumbers(n) {
  * @param {Array} arr 
  */
 const removeEveryOther = (arr) => arr.filter((x, y) => y % 2 == 0)
+
+
+
+/**
+ * NUMBER TO STRING
+ * The code gives an error!
+ * a = 123.toString
+ * Fix it!
+ */
+const a = `${123}`;
