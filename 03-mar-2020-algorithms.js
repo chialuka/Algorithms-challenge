f8ff0b4ec1f81094b98b706f49688923a6744d4e
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-26 00:00:42 +0100

    bartender drinks

--- 03-mar-2020-algorithms.js
+++ 03-mar-2020-algorithms.js
@@ -449,3 +449,34 @@ const removeEveryOther = (arr) => arr.filter((x, y) => y % 2 == 0)
  * Fix it!
  */
 const a = `${123}`;
+
+
+
+/**
+ * BARTENDER, DRINKS!
+ * Write a function getDrinkByProfession/get_drink_by_profession() that receives as input parameter a string, 
+ * and produces outputs according to the following table:
+ * Input	      Output
+ * "Jabroni"	"Patron Tequila"
+ * "School Counselor"	"Anything with Alcohol"
+ * "Programmer"	 "Hipster Craft Beer"
+ * "Bike Gang Member"	"Moonshine" 
+ * "Politician"	"Your tax dollars" 
+ * "Rapper"	"Cristal" 
+ * *anything else*	"Beer" 
+ * Note: anything else is the default case: if the input to the function is not any of the values in the table, 
+ * then the return value should be "Beer."
+ * Make sure you cover the cases where certain words do not show up with correct capitalization.
+ * @param {String} param 
+ */
+function getDrinkByProfession(param){
+  const obj = {
+    'jabroni': 'Patron Tequila',
+    'school counselor': 'Anything with Alcohol',
+    'programmer': 'Hipster Craft Beer',
+    'bike gang member': 'Moonshine',
+    'politician': 'Your tax dollars',
+    'rapper': 'Cristal'
+  }
+  return obj[param.toLowerCase()] ? obj[param.toLowerCase()] : 'Beer'
+}
