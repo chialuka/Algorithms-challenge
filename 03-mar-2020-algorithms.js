4f9a56f7bfec071b0470cf3d0a80b2cbacb32b23
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-03-29 23:21:12 +0100

    true for just one

--- 03-mar-2020-algorithms.js
+++ 03-mar-2020-algorithms.js
@@ -534,3 +534,16 @@ function twosDifference(input){
   }, []);
   return arr;
  }
+
+
+
+ /**
+  * TRUE FOR JUST ONE
+  * Create a function called one that accepts two params:
+  * a sequence
+  * a function
+  * and returns true only if the function in the params returns true for exactly one item in the sequence.
+  * @param {Array} arr 
+  * @param {Function} fun 
+  */
+ const one = (arr, fun) => arr.filter(fun).length == 1;
