03dab91127d023f028f7f1001a4af7e5cc9d3c60
Chukwualuka Chiama <lukaschiama@gmail.com>
2020-04-06 23:57:49 +0100

    fill the hard drive

--- 04-apr-2020-algorithms.js
+++ 04-apr-2020-algorithms.js
@@ -64,3 +64,30 @@ function exchangeWith(a, b) {
   b.pop();
   b.push(...temp[1]);
 }
+
+
+
+/**
+ * FILL THE HARD DRIVE
+ * Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive.
+ * Input:
+ * Array of file sizes (0 <= s <= 100)
+ * Capacity of the HD (0 <= c <= 500)
+ * Output:
+ * Number of files that can be fully saved in the HD
+ * @param {Array} sizes 
+ * @param {Number} hd 
+ */
+function save(sizes, hd) {
+  let count = 0;
+  let val = sizes[0];
+  for (let i = 1; i <= sizes.length; i++) {
+    if (val > hd || count == sizes.length) {
+      return count;
+    } else {
+      val += sizes[i];
+      count++
+    }
+  }
+  return count;
+}
